5066ba6c82c97b6e6f375e746d50a94dde633764
Malwine <malwine@mailbox.org>
2016-01-22 17:41:28 +0100

    Triangle project 2

--- triangle.rb
+++ triangle.rb
@@ -13,14 +13,23 @@
 # and
 #   about_triangle_project_2.rb
 #
+
+class TriangleError <  StandardError
+end
+
 def triangle(a, b, c)
-  if (a == b) && (b == c)
+  
+  if a <= 0 || b <= 0 || c <= 0
+    raise TriangleError.new("TriangleError")
+  elsif a + b <= c || c + b <= a || a + c <= b
+    raise TriangleError.new("TriangleError")
+  elsif (a == b) && (b == c)
     :equilateral
   elsif (a == b) || (b == c) || (a == c)
     :isosceles
   else
     :scalene
-  end  
+  end 
 end
 
 # Error class used in part 2.  No need to change this code.
